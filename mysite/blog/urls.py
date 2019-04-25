09f942ed42a2c0ae999e8964efe4616c1c8b8bd6
fengguo006436 <fengguo006436@sohu-inc.com>
2019-04-25 19:58:20 +0800

    增加CBV视图

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('posts/', views.post_list, name='post_list'),
+    # path('posts/', views.post_list, name='post_list'),
+    path('posts/', views.PostListView.as_view(),),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='detail'),
 
 ]
